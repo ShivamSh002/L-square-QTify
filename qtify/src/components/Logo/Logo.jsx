6fc5a6aa151f670c030dd74f11307175edbf7c42
ShivamSh002 <shivamsharma98879@gmail.com>
2024-01-24 17:05:27 +0530

    committed

---
+++ qtify/src/components/Logo/Logo.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import LogoImg from "../../assets/LogoImg.png"
+
+const Logo = () => {
+  return (
+    <img src={LogoImg} alt="logo" width={67} />
+  )
+}
+
+export default Logo
---
+++ qtify/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Logo from '../Logo/Logo'
+import styles from "./Navbar.module.css"
+import Button from '../Button/Button'
+import Searchbox from '../Searchbox/Searchbox'
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+        <Logo/>
+        <Searchbox />
+        <Button text="Give Feedback"/>
+    </nav>
+  )
+}
+
+export default Navbar
